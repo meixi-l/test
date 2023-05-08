024aba169127de738b639e212c0cc1c9b75ab4c2
Mei Xi <mynameismeixi@gmail.com>
2023-05-08 14:04:39 +0800

    Update table-navigation-javascript-2.js

--- table-navigation-javascript-2.js
+++ table-navigation-javascript-2.js
@@ -6,111 +6,121 @@ setTimeout(() => {
     document.querySelectorAll('.article-table-responsive').forEach((tableContainer) => {
         tableContainer.classList.remove('article-table-responsive');
     });
+
+    document.querySelectorAll('.left-arrow, .right-arrow, .table-gradient-overlap-left, .table-gradient-overlap-right').forEach((element) => {
+        element.remove();
+    });
     
     document.querySelectorAll('body.page__article .block-field-blocknodearticlefield-content table').forEach(function(table, count) {
         // for table identifier, unique each table
-        table.parentElement.classList.add(`article-table-responsive-script-${count}`);
+        const tableParent = table.parentElement;
+        tableParent.classList.add(`article-table-responsive-script-${count}`);
         
         // add same class to all tables, for styling
-        table.parentElement.parentElement.classList.add("article-table-responsive-script-container"); 
-        table.parentElement.classList.add("article-table-responsive-script");
+        tableParent.parentElement.classList.add("article-table-responsive-script-container"); 
+        tableParent.classList.add("article-table-responsive-script");
         
         // determine if there is overflow
-        if(table.offsetWidth < table.scrollWidth) {
+        if(tableParent.offsetWidth < tableParent.scrollWidth) {
+            // append two <div>s for fade effect
+            const divGradientRight = document.createElement("div");
+            divGradientRight.setAttribute("class", `gradient-hidden table-gradient-overlap-right-script table-gradient-overlap-right-script-${count}`);
+            divGradientRight.setAttribute("id", "table-gradient-overlap-right-script-id");
+            tableParent.parentElement.appendChild(divGradientRight);
+            const divGradientLeft = document.createElement("div");
+            divGradientLeft.setAttribute("class", `gradient-hidden table-gradient-overlap-left-script table-gradient-overlap-left-script-${count}`);
+            divGradientLeft.setAttribute("id", "table-gradient-overlap-left-script-id");
+            tableParent.parentElement.appendChild(divGradientLeft);
+
             // insert arrows after the table for navigation
-            const btnArrowLeft = document.createElement('span');
-            btnArrowLeft.setAttribute('id', "btn-slide-left");;
-            btnArrowLeft.setAttribute("class", `btn-hidden left-arrow left-arrow-${count}`);
-            table.parentElement.appendChild(btnArrowLeft);
             const btnArrowRight = document.createElement("span");
             btnArrowRight.setAttribute("id", "btn-slide-right");;
-            btnArrowRight.setAttribute("class", `btn-hidden right-arrow right-arrow-${count}`);
-            table.parentElement.appendChild(btnArrowRight);
+            btnArrowRight.setAttribute("class", `btn-hidden right-arrow-script right-arrow-script-${count}`);
+            tableParent.parentElement.appendChild(btnArrowRight);
+            const btnArrowLeft = document.createElement('span');
+            btnArrowLeft.setAttribute('id', "btn-slide-left");;
+            btnArrowLeft.setAttribute("class", `btn-hidden left-arrow-script left-arrow-script-${count}`);
+            tableParent.parentElement.appendChild(btnArrowLeft);
 
-            // append two <div>s for fade effect
-            const divGradientLeft = document.createElement("div");
-            divGradientLeft.setAttribute("class", `gradient-hidden table-gradient-overlap-left table-gradient-overlap-left-${count}`);
-            divGradientLeft.setAttribute("id", "table-gradient-overlap-left-id");
-            table.parentElement.appendChild(divGradientLeft);
-            const divGradientRight = document.createElement("div");
-            divGradientRight.setAttribute("class", `gradient-hidden table-gradient-overlap-right table-gradient-overlap-right-${count}`);
-            divGradientRight.setAttribute("id", "table-gradient-overlap-right-id");
-            table.parentElement.appendChild(divGradientRight);
         
             // Declare variables for container, max scroll left, left and right buttons
             // var container = $('.article-table-responsive-script-'+count)[0];
-            const maxScrollLeft = table.scrollWidth - table.clientWidth;
+            const maxScrollLeft = tableParent.scrollWidth - tableParent.clientWidth;
             // const btnSlideLeft = $(`.left-arrow-${count}`);
             // const btnSlideRight = $(`.right-arrow-${count}`);
-            // const tableGradientOverlapLeft = $(`.table-gradient-overlap-left-${count}`);
-            // const tableGradientOverlapRight = $(`.table-gradient-overlap-right-${count}`);
+            // const tableGradientOverlapLeft = $(`.table-gradient-overlap-left-script-${count}`);
+            // const tableGradientOverlapRight = $(`.table-gradient-overlap-right-script-${count}`);
         
             // Right Arrow Click
             btnArrowRight.addEventListener('click', function() {
-                console.log('right arrow button clicked');
-            sideScroll(table, 'right', 25, 100, 10);
+                console.log('right arrow button clicked from embed script');
+                sideScrollScript(tableParent, 'right', 25, 100, 10);
 
-            // If scrolled to right and there is more to scroll
-            if(table.scrollLeft >= 0) {
-                btnArrowLeft.classList.remove("btn-hidden");
-                divGradientLeft.classList.remove("gradient-hidden");
-            }
-            // If scrolled to right and max is reached
-            if(table.scrollLeft > maxScrollLeft-50) {
-                btnArrowRight.classList.add("btn-hidden");
-                divGradientRight.classList.add("gradient-hidden");
-            }
+                // If scrolled to right and there is more to scroll
+                if(tableParent.scrollLeft >= 0) {
+                    btnArrowLeft.classList.remove("btn-hidden");
+                    divGradientLeft.classList.remove("gradient-hidden");
+                }
+                // If scrolled to right and max is reached
+                if(tableParent.scrollLeft > maxScrollLeft-50) {
+                    btnArrowRight.classList.add("btn-hidden");
+                    divGradientRight.classList.add("gradient-hidden");
+                }
             });
         
             // Left Arrow Click
             btnArrowLeft.addEventListener('click', function() {
-                console.log('left arrow button clicked');
-            sideScroll(table, 'left', 25, 50, 10);
+                console.log('left arrow button clicked from embed script');
+                sideScrollScript(tableParent, 'left', 25, 50, 10);
 
-            if(table.scrollLeft < 23){
-                btnArrowLeft.classList.add("btn-hidden");
-                divGradientLeft.classList.add("gradient-hidden");
-            }
-            if(table.scrollLeft < maxScrollLeft-1) {
-                btnArrowRight.classList.remove("btn-hidden");
-                divGradientRight.classList.remove("gradient-hidden");
-            }
+                if(tableParent.scrollLeft < 23){
+                    btnArrowLeft.classList.add("btn-hidden");
+                    divGradientLeft.classList.add("gradient-hidden");
+                }
+                if(tableParent.scrollLeft < maxScrollLeft-1) {
+                    btnArrowRight.classList.remove("btn-hidden");
+                    divGradientRight.classList.remove("gradient-hidden");
+                }
             });
         
             // Scroll Function on click
-            function sideScroll(element, direction, speed, distance, step) {
-            var scrollAmount = 0;
-            var slideTimer = setInterval(function () {
-                if (direction == 'left') {
-                element.scrollLeft -= step * 2;
-                } else {
-                element.scrollLeft += step * 1;
-                }
-                scrollAmount += step;
-                if (scrollAmount >= distance) {
-                window.clearInterval(slideTimer);
-                }
-            }, speed);
+            function sideScrollScript(element, direction, speed, distance, step) {
+                var scrollAmount = 0;
+                var slideTimer = setInterval(function () {
+                    if (direction == 'left') {
+                    element.scrollLeft -= step * 2;
+                    } else {
+                    element.scrollLeft += step * 1;
+                    }
+                    scrollAmount += step;
+                    if (scrollAmount >= distance) {
+                    window.clearInterval(slideTimer);
+                    }
+                }, speed);
             }
         
-            table.onscroll = ()=>{
-            if(table.scrollLeft >= 0) {
-                btnArrowLeft.classList.remove("btn-hidden");
-                divGradientLeft.classList.remove("gradient-hidden");
+            function updateTableParent() {
+                if(tableParent.scrollLeft >= 0) {
+                    btnArrowLeft.classList.remove("btn-hidden");
+                    divGradientLeft.classList.remove("gradient-hidden");
+                }
+                if(tableParent.scrollLeft > maxScrollLeft-1) {
+                    btnArrowRight.classList.add("btn-hidden");
+                    divGradientRight.classList.add("gradient-hidden");
+                }
+                if(tableParent.scrollLeft < 23){
+                    btnArrowLeft.classList.add("btn-hidden");
+                    divGradientLeft.classList.add("gradient-hidden");
+                }
+                if(tableParent.scrollLeft < maxScrollLeft-1) {
+                    btnArrowRight.classList.remove("btn-hidden");
+                    divGradientRight.classList.remove("gradient-hidden");
+                }
             }
-            if(table.scrollLeft > maxScrollLeft-1) {
-                btnArrowRight.classList.add("btn-hidden");
-                divGradientRight.classList.add("gradient-hidden");
+            tableParent.onscroll = ()=>{
+                updateTableParent();
             }
-            if(table.scrollLeft < 23){
-                btnArrowLeft.classList.add("btn-hidden");
-                divGradientLeft.classList.add("gradient-hidden");
-            }
-            if(table.scrollLeft < maxScrollLeft-1) {
-                btnArrowRight.classList.remove("btn-hidden");
-                divGradientRight.classList.remove("gradient-hidden");
-            }
-            }
+            updateTableParent();
         }
     });
 }, 3000);
