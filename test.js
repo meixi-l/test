49f1158d8f3e1f661bed10ee415369c30ea487aa
Mei Xi <mynameismeixi@gmail.com>
2023-05-08 16:13:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 (function hello1() {
     console.log("hello");
-    [...document.getElementsByClassName('test-paragraph')].forEach((element) => element.style.color = 'red');
+    for (i = 0; i < testParagraphs.length; i++) {
+        testParagraphs[i].style.color = 'red';
+    }
     document.querySelector("#test-paragraph").style.color = "orange";
 })();
